5983f294631a0136d88e3044542e5431464f883c
maheshg11 <maheshghumare1234@gmail.com>
2026-01-13 23:18:55 +0530

    Chore: Restructure

--- gRPCinstall.sh
+++ gRPCinstall.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
 #Installations as mentioned on gRPC website for C++
-export MY_INSTALL_DIR=$HOME/.local
+export MY_INSTALL_DIR=/usr/local
 mkdir -p $MY_INSTALL_DIR
 
-export PATH="$MY_INSTALL_DIR/bin:$PATH"
-apt update && apt-get install -y build-essential git protobuf-compiler autoconf libtool pkg-config sudo wget
+apt update && apt-get install -y build-essential git autoconf libtool pkg-config sudo wget
 wget -O cmake-linux.sh https://github.com/Kitware/CMake/releases/download/v3.30.3/cmake-3.30.3-linux-x86_64.sh
 sh cmake-linux.sh -- --skip-license --prefix=$MY_INSTALL_DIR
 rm cmake-linux.sh
--- key_value_store/protofiles/generate_cpp.sh
+++
@@ -1,2 +0,0 @@
-protoc -I . --grpc_out=. --plugin=protoc-gen-grpc=$HOME/.local/bin/grpc_cpp_plugin gRPC_Communication.proto
-protoc -I . --cpp_out=. gRPC_Communication.proto
